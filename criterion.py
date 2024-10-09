f4c24d27ae9951fb36a15db87384129f5e949b42
Laurens Hoogenboom <hoogenboom.l.p@gmail.com>
2024-10-09 12:18:19 +0200

    criterion copied from example

---
+++ criterion.py
@@ -0,0 +1,14 @@
+import torch as tt
+import torch.nn as nn
+import torch.nn.functional as ff
+
+
+class Criterion(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, prediction, reference):
+        loss = ff.l1_loss(prediction, reference)
+        return loss
+
+	# Define more criteria for different networks later
